51ec816c79ad16bcdbcbfd124996c3809fa13513
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-23 16:29:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ bot.on('raw', event => {
 		if(msg.author.id == bot.user.id && msg.content != initialMessage1){
 			
 			var re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            		var role = msg.content.match(re);
+            		var role = msg.content.match(re)[1];
 			
 			if(user.id != bot.user.id){
 			var roleObj = msg.guild.roles.find('name', role);
