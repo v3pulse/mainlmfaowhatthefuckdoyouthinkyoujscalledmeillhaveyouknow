4bff338b3c8e90b79dbe9f905da7699c2a30d11b
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-23 16:25:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -158,27 +158,31 @@ bot.on('raw', event => {
 		const initialMessage1 = `**React below based on your region.**`;
 
 	if(event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+		
 	let channel = bot.channels.get(event.d.channel_id);
 	let message = channel.fetchMessage(event.d.message_id).then(msg => {
 	let user = msg.guild.members.get(event.d.user_id);
+		
 		if(msg.author.id == bot.user.id && msg.content != initialMessage1){
+			
 			var re = `\\*\\*"(.+)?(?="\\*\\*)`;
 			var role = msg.content.match(re)[1];
 			
 			if(user.id != bot.user.id){
 			var roleObj = msg.guild.roles.find('name', role);
 			var memberObj = msg.guild.members.get(user.id);
-			if(user.roles.has(msg.guild.roles.find(r => r.name === "NA-E"))) return;
-			if(user.roles.has(msg.guild.roles.find(r => r.name === "NA-W"))) return;
-			if(user.roles.has(msg.guild.roles.find(r => r.name === "EU"))) return;
+			
+// 			if(user.roles.has(msg.guild.roles.find(r => r.name === "NA-E"))) return;
+// 			if(user.roles.has(msg.guild.roles.find(r => r.name === "NA-W"))) return;
+// 			if(user.roles.has(msg.guild.roles.find(r => r.name === "EU"))) return;
 				
 			if(event.t === "MESSAGE_REACTION_ADD"){
 			memberObj.addRole(roleObj)
 			}else{
 			memberObj.removeRole(roleObj);
 			}
-			}
 		}
+	}
 	})
 	
 	
